42ac858281c1d555f1bb7d88ead8f24c4ec1f86d
Zach <zrkier@email.com>
2026-09-21 19:02:05 -0500

    Task 1 updated

--- proj1-code/swish.c
+++ proj1-code/swish.c
@@ -14,17 +14,20 @@
 #define CMD_LEN 512
 #define PROMPT "@> "
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Task 4: Set up shell to ignore SIGTTIN, SIGTTOU when put in background
     // You should adapt this code for use in run_command().
     struct sigaction sac;
     sac.sa_handler = SIG_IGN;
-    if (sigfillset(&sac.sa_mask) == -1) {
+    if (sigfillset(&sac.sa_mask) == -1)
+    {
         perror("sigfillset");
         return 1;
     }
     sac.sa_flags = 0;
-    if (sigaction(SIGTTIN, &sac, NULL) == -1 || sigaction(SIGTTOU, &sac, NULL) == -1) {
+    if (sigaction(SIGTTIN, &sac, NULL) == -1 || sigaction(SIGTTOU, &sac, NULL) == -1)
+    {
         perror("sigaction");
         return 1;
     }
@@ -36,32 +39,49 @@ int main(int argc, char **argv) {
     char cmd[CMD_LEN];
 
     printf("%s", PROMPT);
-    while (fgets(cmd, CMD_LEN, stdin) != NULL) {
+    while (fgets(cmd, CMD_LEN, stdin) != NULL)
+    {
         // Need to remove trailing '\n' from cmd. There are fancier ways.
         int i = 0;
-        while (cmd[i] != '\n' && cmd[i] != '\0') {
+        while (cmd[i] != '\n' && cmd[i] != '\0')
+        {
             i++;
         }
         cmd[i] = '\0';
 
-        if (tokenize(cmd, &tokens) != 0) {
+        if (tokenize(cmd, &tokens) != 0)
+        {
             printf("Failed to parse command\n");
             strvec_clear(&tokens);
             job_list_free(&jobs);
             return 1;
         }
-        if (tokens.length == 0) {
+        if (tokens.length == 0)
+        {
             printf("%s", PROMPT);
             continue;
         }
         const char *first_token = strvec_get(&tokens, 0);
 
-        if (strcmp(first_token, "pwd") == 0) {
+        if (strcmp(first_token, "pwd") == 0)
+        {
             // TODO Task 1: Print the shell's current working directory
             // Use the getcwd() system call
+
+            char curDir[CMD_LEN];
+
+            if (getcwd(curDir, CMD_LEN) == NULL)
+            {
+                perror("getcwd");
+            }
+            else
+            {
+                printf("%s\n", curDir);
+            }
         }
 
-        else if (strcmp(first_token, "cd") == 0) {
+        else if (strcmp(first_token, "cd") == 0)
+        {
             // TODO Task 1: Change the shell's current working directory
             // Use the chdir() system call
             // If the user supplied an argument (token at index 1), change to that directory
@@ -69,20 +89,26 @@ int main(int argc, char **argv) {
             // This is available in the HOME environment variable (use getenv())
         }
 
-        else if (strcmp(first_token, "exit") == 0) {
+        else if (strcmp(first_token, "exit") == 0)
+        {
             strvec_clear(&tokens);
             break;
         }
 
         // Task 5: Print out current list of pending jobs
-        else if (strcmp(first_token, "jobs") == 0) {
+        else if (strcmp(first_token, "jobs") == 0)
+        {
             int i = 0;
             job_t *current = jobs.head;
-            while (current != NULL) {
+            while (current != NULL)
+            {
                 char *status_desc;
-                if (current->status == BACKGROUND) {
+                if (current->status == BACKGROUND)
+                {
                     status_desc = "background";
-                } else {
+                }
+                else
+                {
                     status_desc = "stopped";
                 }
                 printf("%d: %s (%s)\n", i, current->name, status_desc);
@@ -92,34 +118,43 @@ int main(int argc, char **argv) {
         }
 
         // Task 5: Move stopped job into foreground
-        else if (strcmp(first_token, "fg") == 0) {
-            if (resume_job(&tokens, &jobs, 1) == -1) {
+        else if (strcmp(first_token, "fg") == 0)
+        {
+            if (resume_job(&tokens, &jobs, 1) == -1)
+            {
                 printf("Failed to resume job in foreground\n");
             }
         }
 
         // Task 6: Move stopped job into background
-        else if (strcmp(first_token, "bg") == 0) {
-            if (resume_job(&tokens, &jobs, 0) == -1) {
+        else if (strcmp(first_token, "bg") == 0)
+        {
+            if (resume_job(&tokens, &jobs, 0) == -1)
+            {
                 printf("Failed to resume job in background\n");
             }
         }
 
         // Task 6: Wait for a specific job identified by its index in job list
-        else if (strcmp(first_token, "wait-for") == 0) {
-            if (await_background_job(&tokens, &jobs) == -1) {
+        else if (strcmp(first_token, "wait-for") == 0)
+        {
+            if (await_background_job(&tokens, &jobs) == -1)
+            {
                 printf("Failed to wait for background job\n");
             }
         }
 
         // Task 6: Wait for all background jobs
-        else if (strcmp(first_token, "wait-all") == 0) {
-            if (await_all_background_jobs(&jobs) == -1) {
+        else if (strcmp(first_token, "wait-all") == 0)
+        {
+            if (await_all_background_jobs(&jobs) == -1)
+            {
                 printf("Failed to wait for all background jobs\n");
             }
         }
 
-        else {
+        else
+        {
             // TODO Task 2: If the user input does not match any built-in shell command,
             // treat the input as a program name and command-line arguments
             // USE THE run_command() FUNCTION DEFINED IN swish_funcs.c IN YOUR IMPLEMENTATION
